9b2f96f80d5f3fe0d9a06fa438e2127b44b2406e
lukostelloe <lukostelloe@gmail.com>
2021-06-23 11:47:20 +0200

    fixing some css

--- js/mouseover.js
+++ js/mouseover.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector(".hero");
 const text = hero.querySelector(".pizza");
-const move = 400;
+const move = 50;
 
 function effect(e) {
   ////////////////////////////////convert size of hero div to variable///////////////////////////////
@@ -19,12 +19,12 @@ function effect(e) {
 
   text.style.textShadow = `
   ${xMove}px ${yMove}px 2px blue,
-  ${xMove * -1}px ${yMove}px 2px red,
-  ${yMove}px ${xMove * -1}px 2px yellow,
-  ${yMove * -1}px ${xMove}px 2px green
+  ${xMove * -1}px ${yMove}px 2px red
   `;
 }
 
+// ${yMove}px ${xMove * -1}px 2px yellow,
+// ${yMove * -1}px ${xMove}px 2px green
 /////////////////////////////////////////////////////listen for mouse move, call function///////////////////////////////////////
 
 hero.addEventListener("mousemove", effect);
